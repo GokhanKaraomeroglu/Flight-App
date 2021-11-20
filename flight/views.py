2909f8b07d95f5db07606c9cb68a7144c0203433
Himmel König <itogreniyorum@gmail.com>
2021-11-20 23:12:22 +0300

    Reservation

--- flight/views.py
+++ flight/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Flight, Reservation, Passenger
+from .serializers import FlightSerializer, ReservationSerializer
+from rest_framework import viewsets
+from .permission import IsStuffOrReadOnly
 
+
+class FlightView(viewsets.ModelViewSet):
+  queryset = Flight.objects.all()
+  serializer_class = FlightSerializer
+  permission_classes = (IsStuffOrReadOnly,)
+  
+class ReservationView(viewsets.ModelViewSet):
+  queryset = Reservation.objects.all()
+  serializer_class = ReservationSerializer
+
+  
 # Create your views here.
